630581504916e403d74816fbc2a8a3c153d1eaf9
ahmed abdelaziz <ahmed.abdelaziz3080@gmail.com>
2024-06-30 05:49:48 +0300

    _ Added Login cubit and state - Added some strings - Finished home screen UI - Added dark mode

---
+++ lib/view_model/cubit/login/login_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  bool isPasswordVisible = false ;
+
+
+
+  void togglePasswordVisibility() {
+    isPasswordVisible = !isPasswordVisible;
+    emit(LoginPasswordVisibilityToggled(isPasswordVisible));
+  }
+
+  Future<void> login() async {
+    emit(LoginLoading());
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (emailController.text == "test@gmail.com" && passwordController.text == "admin123") {
+      emit(LoginSuccess());
+    } else {
+      emit(LoginFailure("Invalid email or password"));
+    }
+  }
+
+  @override
+  Future<void> close() {
+    emailController.dispose();
+    passwordController.dispose();
+    return super.close();
+  }
+
+}
---
+++ lib/view_model/cubit/login/login_state.dart
@@ -0,0 +1,21 @@
+part of 'login_cubit.dart';
+
+@immutable
+sealed class LoginState {}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoading extends LoginState {}
+
+final class LoginSuccess extends LoginState {}
+
+final class LoginFailure extends LoginState {
+  final String error;
+  LoginFailure(this.error);
+}
+
+final class LoginPasswordVisibilityToggled extends LoginState {
+  final bool isPasswordVisible;
+
+  LoginPasswordVisibilityToggled(this.isPasswordVisible);
+}
