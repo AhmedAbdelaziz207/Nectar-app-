257360480caa7fbb4ad6cca4c439bbd728c660f7
ahmed abdelaziz <ahmed.abdelaziz3080@gmail.com>
2024-07-03 02:38:02 +0300

    - Finished Signup screen Cubit

---
+++ lib/view_model/utils/constants/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAssets{
+  static const appLogo = "assets/icons/app_logo.svg";
+  static const splashIcon = "assets/icons/splash_icon.png";
+  static const onboardingImage = "assets/images/onboarding_bg.png";
+  static const homeBanner = "assets/images/home_banner.png";
+  static const productIcon = "assets/icons/product_icon.png";
+  static const translations = "assets/translations";
+
+}
